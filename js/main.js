05857dbbca6f11ca0941f93c98c82a745f1c08a3
yolisett <lizbeth9589@gmail.com>
2017-02-26 23:38:33 -0500

    Arreglo en mi codigo de jquery

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ function validateForm(){
         var apellido = $('#lastname').val();
         var email = $('#email').val();
         var password = $('#password').val();
+   //el name tiene que tener los caracteres designados
 	if (/^[A-Z][a-z]+$/.test(nombre) === false) {
         var message = "Es incorrecto, debe tener la primera letra mayúscula";
         createSpan('name', message);
@@ -15,6 +16,7 @@ function validateForm(){
     else {
         $('.name span').remove();
     }
+    //el lastname tiene que tener los caracteres designados
 	if (/^[A-Z][a-z]+$/.test(apellido) === false) {
         var message = "Es incorrecto, debe tener la primera letra mayúscula";
         createSpan('lastname', message);
@@ -22,6 +24,7 @@ function validateForm(){
     else {
         $('.lastname span').remove();
     }
+    //el email tiene que tener los caracteres designados
 	if (/^\S+@\S+\.\S+$/.test(email) === false) {
         var message = "email incorrecto";
         createSpan('email', message);
@@ -29,6 +32,7 @@ function validateForm(){
     else {
         $('.email span').remove();
     }
+    //el password tiene que tener los caracteres designados
     if (password.length>=6 === false || (/^[0-9]+$/.test(password) !== false)|| (/^[a-zA-Z]+$/.test(password) !== false) ) {
         var message = "El password debe tener 6 carácteres con letras y números";
         createSpan('password', message);
